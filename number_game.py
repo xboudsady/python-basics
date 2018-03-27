0f021b050b5663b9a2d38b93ff7775f51c6f9c7b
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-03-26 23:12:16 -0700

    Added repeat game function

--- number_game.py
+++ number_game.py
@@ -1,15 +1,41 @@
 import random
+# https: // www..com/view_video.php?viewkey = 785027296
 
-# generate a random number bewteen 1 and 10
-secret_num = random.randint(1, 10)
+# safely make an int 
+# limit guesses
+# too high message
+# too low message
+# play again
 
-while True:
-    # get a number guess from the player
-    guess = int(input("Guess a number between 1 and 10: "))
+def game():
+    # generate a random number bewteen 1 and 10
+    secret_num = random.randint(1, 10)
+    guesses = []
 
-    # compare guess to secret number
-    if guess == secret_num:
-        print("You got it! My number was {}".format(secret_num))
-        break
+    while len(guesses) < 5:
+        try:
+            # get a number guess from the player
+            guess = int(input("Guess a number between 1 and 10: "))
+        except ValueError:
+            print("{} isn't a number!".format(guess))
+        else:
+            # compare guess to secret number
+            if guess == secret_num:
+                print("You got it! My number was {}".format(secret_num))
+                break
+            elif guess < secret_num:
+                print("My number is higher than {}".format(guess))
+            else:
+                print("My number is lower than {}".format(guess))
+            guesses.append(guess)
     else:
-        print("That's not it!")
+        print("You didn't get it! My number was {}".format(secret_num))
+    
+    play_again = input("Do you want to play again? Y/n ")
+    
+    if play_again.lower() != 'n':
+        game()
+    else:
+        print("Bye!")
+
+game()
